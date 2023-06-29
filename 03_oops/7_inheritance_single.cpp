e9aa0770a289230606db0be6d60db535808d1b9a
Ravi <raviranjanwrs12@gmail.com>
2023-06-29 17:46:15 +0530

    DSA C++

---
+++ 03_oops/7_inheritance_single.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Animal{
+    public:
+    void eat(){
+        cout<<"eating"<<endl;
+    }
+};
+class Dog:public Animal{
+    public:
+    void bark(){
+        cout<<"barking"<<endl;
+    }
+};
+int main(){
+    Dog obj;
+    obj.eat();
+    obj.bark();
+    return 0;
+}
